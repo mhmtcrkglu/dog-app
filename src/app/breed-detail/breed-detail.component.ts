b6cfb83f41e09594b19c07fc3958a3af16616cbc
System Administrator <root@Mehmet-MBP.local>
2024-07-28 03:30:24 +0200

    Add breed-detail component

---
+++ src/app/breed-detail/breed-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breed-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './breed-detail.component.html',
+  styleUrl: './breed-detail.component.css'
+})
+export class BreedDetailComponent {
+
+}
